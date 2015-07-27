1608da986dd0c88998bebbeab3edbed4f0bb728e
callmedddd <MLS@2015-099deMacBook-Pro.local>
2015-07-27 14:26:36 +0800

    添加图片
    添加头像和button

--- follower/follower.js
+++ follower/follower.js
@@ -1,43 +1,43 @@
 var user = [
     {
-        name : 'a',
-        description : 'AAAA'
+        name : 'Barack Obama',
+        description : 'Barack Obama Barack Obama Barack Obama '
     },
     {
-        name : 'b',
-        description : 'BBBB'
+        name : 'CNN',
+        description : 'CNNCNNCNNCNNCNNCNN'
     },
     {
-        name : 'c',
-        description : 'CCCC'
+        name : 'Ellen',
+        description : 'EllenEllenEllenEllenEllen'
     },
     {
-        name : 'd',
-        description : 'DDDD'
+        name : 'GitHub',
+        description : 'GitHubGitHubGitHubGitHubGitHub'
     },
     {
-        name : 'e',
-        description : 'EEEE'
+        name : 'Google Play',
+        description : 'Google PlayPlayPlayPlayPlay'
     },
     {
-        name : 'f',
-        description : 'FFFF'
+        name : 'google',
+        description : 'googlegooglegooglegooglegoogle'
     },
     {
-        name : 'g',
-        description : 'GGGG'
+        name : 'ifanr',
+        description : 'ifanrifanrifanrifanrifanr'
     },
     {
-        name : 'h',
-        description : 'HHHH'
+        name : 'TED talks',
+        description : 'TED talksTED talksTED talksTED talks'
     },
     {
-        name : 'i',
-        description : 'IIII'
+        name : 'twitter',
+        description : 'twittertwittertwittertwitter'
     },
     {
-        name : 'j',
-        description : 'JJJJ'
+        name : 'YouTube',
+        description : 'YouTubeYouTubeYouTubeYouTube'
     }
 ];
 fowller = function(index,bol,ctx){
@@ -53,6 +53,10 @@ getTpl = function(selector){
         }
 },
 createOne = function(len,arr){  
+    // var values = user.filter(function(iterm,index,array){
+    //     return !(index in arr)
+    // });
+    // console.log(values);
     var a = Math.floor((len)*Math.random());
     if(arr){
         for(var i = 0;i < arr.length;i++){
@@ -83,24 +87,28 @@ replaceTpl = function(tpl,fowllers,n){
 },
 addFowlers = function(total){    
     var result = createMore(total);
-    var fowllers = [];
     result.forEach(function(iterm,index){
         fowllers.push(new fowller(iterm,true,user[iterm]))
     });
+    console.log(fowllers,'first');
     ul.innerHTML = replaceTpl(tpl,fowllers);
     return fowllers;
 },
-addOneFowler = function(num,arr){
+followersToindex = function(){
     var filter = [];
-    arr.forEach(function(iterm,index,arr){
+    fowllers.forEach(function(iterm,index,fowllers){
         filter.push(iterm.index);
-    }) 
+    })
+    return filter;
+}
+addOneFowler = function(num){
+    var filter = followersToindex(fowllers);
     var li = document.createElement('li');
     if(filter.length < user.length){       
         var q = createOne(user.length,filter);
-        arr.push(new fowller(q,true,user[q])); 
+        fowllers.push(new fowller(q,true,user[q])); 
         li.className = 'iterm-'+num+' iterm';
-        li.innerHTML = replaceTpl(tpl_li,[arr[arr.length-1]],num);       
+        li.innerHTML = replaceTpl(tpl_li,[fowllers[fowllers.length-1]],num);       
     }else{
         li.className = 'showNo';
         li.innerHTML = '没有更多啦';
@@ -109,28 +117,49 @@ addOneFowler = function(num,arr){
     }
     return li;   
 },
-delFowler = function(arr){
+delFowler = function(){
    return function(){
         if(event.target.tagName.toLowerCase() == 'i'){
             var num = event.target.getAttribute("data-i");
-            arr[num].like =false;
+            fowllers[num].like =false;
             ul.removeChild(document.querySelectorAll('.iterm-'+num)[0]);
             if(!end){
-                var new_li = addOneFowler(num,arr);
+                var new_li = addOneFowler(num);
                 ul.appendChild(new_li);
             }            
 
         }
    }
 },
+refresher = function(){
+    var filter = followersToindex(fowllers);
+    var n = (user.length-filter.length)/3>1 ? 3 : (user.length-filter.length)%3;
+    var newArr = [];
+    console.log(n);
+    for(var i =0; i<n;i++){
+            var q = createOne(user.length,filter);
+            fowllers.push(new fowller(q,true,user[q])); 
+            filter = followersToindex(fowllers);
+            newArr.push(fowllers[fowllers.length-1]);
+       
+    }
+    // if(!n){
+        ul.innerHTML = replaceTpl(tpl,newArr);
+    // }
+    
+    
+
+},
 ul = document.querySelector('#follwers'),
 tpl = getTpl('#tpl_content').li.replace(/^\s*/,'').replace(/\s*$/,''),
 tpl_li = getTpl('#tpl_content').liCtx.replace(/^\s*/,'').replace(/\s*$/,''),
-end = false;
+end = false,
+fowllers = [];
 
     (function(){
-        var firstFowllers = addFowlers(3);
-        ul.addEventListener('click',delFowler(firstFowllers),false);
+        ul.addEventListener('click',delFowler(addFowlers(3)),false);
+        var refresh = document.querySelector('#rf');
+        refresh.addEventListener('click',refresher,false);
     })()
 
 
