155c95c6d022cd98a5b53486748b105b7d5a5dca
callmedddd <MLS@2015-099deMacBook-Pro.local>
2015-08-05 11:41:47 +0800

    修改error 与变量命名
    遇到错误不抛错误

--- eventEmiter/eventEmitter.js
+++ eventEmiter/eventEmitter.js
@@ -13,9 +13,8 @@ var isNone = function(obj){
 var namespace = function(eventName){
     var obj = {};
     if(eventName.indexOf('.')!= -1){
-
         obj.main = eventName.split('.')[0];
-        obj.fenzhi = [eventName.split('.')[1]];
+        obj.branch = [eventName.split('.')[1]];
         return obj;
     }else{
         return eventName;
@@ -34,15 +33,15 @@ eventEmitter.prototype.on = function(eventName,fn){
     }else if(typeof _eventName == 'object'){
         if(!this._events[_eventName.main]){
             this._events[_eventName.main] = {};
-            this._events[_eventName.main][_eventName.fenzhi] = fn
+            this._events[_eventName.main][_eventName.branch] = fn
         }else{
-            if(!this._events[_eventName.main][_eventName.fenzhi]){
-                this._events[_eventName.main][_eventName.fenzhi] = fn;
+            if(!this._events[_eventName.main][_eventName.branch]){
+                this._events[_eventName.main][_eventName.branch] = fn;
             }else{
-                if(typeof this._events[_eventName.main][_eventName.fenzhi] == 'function')
+                if(typeof this._events[_eventName.main][_eventName.branch] == 'function')
                     this._events[eventName]  = [this._events[eventName] ,fn];
-                else if(this._events[_eventName.main][_eventName.fenzhi] instanceof Array){
-                    this._events[_eventName.main][_eventName.fenzhi].push(fn)
+                else if(this._events[_eventName.main][_eventName.branch] instanceof Array){
+                    this._events[_eventName.main][_eventName.branch].push(fn)
                 }
             }
         } 
@@ -52,9 +51,8 @@ eventEmitter.prototype.off = function(eventName,fn){
     var _eventName = namespace(eventName);
     if(typeof _eventName == 'string'){
         var cbks = this._events[eventName];
-        if(!cbks){
-            this.error();
-        }else if(cbks instanceof Array || typeof cbks == 'function'){
+        if(!cbks) return;           
+        if(cbks instanceof Array || typeof cbks == 'function'){
             if(typeof cbks == 'function'){
                 delete this._events[eventName];
             }else{
@@ -62,18 +60,17 @@ eventEmitter.prototype.off = function(eventName,fn){
                 cbks.splice(posi,1);
             }
         }else if(typeof cbks == 'object' && !isNone(cbks)){
-            for(var fenzhi in cbks){
-                delete this._events[eventName][fenzhi];
+            for(var branch in cbks){
+                delete this._events[eventName][branch];
             }
             delete this._events[eventName];
         }
 
     }else if(typeof _eventName == 'object'){
-        var cbks = this._events[_eventName.main][_eventName.fenzhi];
-        if(!cbks){
-            this.error();
-        }else if(typeof cbks == 'function'){
-            delete this._events[_eventName.main][_eventName.fenzhi];
+        var cbks = this._events[_eventName.main][_eventName.branch];
+        if(!cbks) return;
+        if(typeof cbks == 'function'){
+            delete this._events[_eventName.main][_eventName.branch];
         }else if(cbks instanceof Array){
             var posi = cbks.indexOf(fn);
             cbks.splice(posi,1);
@@ -84,28 +81,24 @@ eventEmitter.prototype.emit = function(eventName){
     var _eventName = namespace(eventName);
     if(typeof _eventName == 'string'){
         var cbks = this._events[eventName];
-        if(!cbks){
-            this.error();
-        }else {
-            if(typeof cbks == 'function') {
-               cbks(); 
+        if(!cbks) return;
+        if(typeof cbks == 'function') {
+           cbks(); 
+        }
+        else if(cbks instanceof Array){
+            for(var i = 0;i<cbks.length;i++){
+                cbks[i]();
             }
-            else if(cbks instanceof Array){
-                for(var i = 0;i<cbks.length;i++){
-                    cbks[i]();
-                }
-            }else if(!isNone(cbks)){
-                for(var fenzhi in cbks){
-                    cbks[fenzhi]();
-                }
+        }else if(!isNone(cbks)){
+            for(var branch in cbks){
+                cbks[branch]();
             }
         }
     }
     else if(typeof _eventName == 'object'){
-        var cbks = this._events[_eventName.main][_eventName.fenzhi];
-        if(!cbks){
-            this.error();
-        }else if(typeof cbks == 'function'){
+        var cbks = this._events[_eventName.main][_eventName.branch];
+        if(!cbks) return;
+        if(typeof cbks == 'function'){
             cbks();
         }else if(cbks instanceof Array){
             for(var i = 0;i<cbks.length;i++){
@@ -122,7 +115,4 @@ eventEmitter.prototype.once = function(eventName,fn){
         fn();
     }
     this.on(eventName,linshi);
-}
-eventEmitter.prototype.error = function(){
-    console.error('该事件未设置回调函数');
 }
