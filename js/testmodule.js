2c856f5dee6577d09f0b753aa5fb83055b18a91e
callmedddd <MLS@2015-099deMacBook-Pro.local>
2015-09-10 11:41:38 +0800

    移动至ModuleLoad

--- js/testmodule.js
+++
@@ -1,7 +0,0 @@
-ModuleLoad.define( 'js/testmodule' , ['js/testmodule2'] , function(require , exports){
-    var testmodule2 = require('js/testmodule2')
-        testmodule2.test();
-    exports.test = function(){
-        console.log(111)
-    }
-})
--- js/testmodule2.js
+++
@@ -1,5 +0,0 @@
-ModuleLoad.define( 'js/testmodule2' , [] , function(require , exports){
-    exports.test = function(){
-        console.log(222)
-    }
-})
